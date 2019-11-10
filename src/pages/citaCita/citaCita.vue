81871e66a15e8d3fe2ee7c661fe855153a444f91
Gina Rizka Alsyah <ginarizkaa@gmail.com>
2019-11-10 12:20:11 +0700

    layout

---
+++ src/pages/citaCita/citaCita.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="q-mt-xl q-pt-xl">
+    <div class="q-px-xl q-pt-xl">
+      <div class="q-mt-xl q-pt-xl">
+        <q-select rounded outlined bg-color="grey-2" v-model="model" :options="options"/>
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="stylus"></style>
+<script>
+export default {
+  data() {
+    return {
+      model: "Pilih...",
+      options: ["Sosiolog", "Dokter", "Psikolog", "Programmer", "Arsitek"]
+    };
+  }
+};
+</script>
