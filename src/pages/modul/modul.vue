adf1d9736c185760dc9d22d45f4dc698a5381693
Gina Rizka Alsyah <ginarizkaa@gmail.com>
2019-12-01 13:11:31 +0700

    huhu

--- src/pages/modul/modul.vue
+++ src/pages/modul/modul.vue
@@ -4,7 +4,7 @@
       <grid
         title="Kelas 9"
         caption="Referensi lengkap soal per materi untuk belajar kamu!"
-        page="/mp/Kelas 9"
+        page="/mp/modul/Kelas 9"
         imgtext="XI"
       />
       <grid
