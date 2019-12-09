908fe6f5a7396480b45f74bcef8359af4469d369
mahpudeen <mahpudeen@gmail.com>
2019-12-09 13:02:29 +0700

    bank Soal fix

---
+++ src/components/bankSoal/cbankSoal.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-card class="bankSoal2" @click="navigate()">
+    <div class="row items-center">
+      <div class="col-9">
+        <q-item>
+          <q-item-section class="txt-left">
+            <q-item-label class="font6 fs35 white ">{{title}}</q-item-label>
+          </q-item-section>
+        </q-item>
+      </div>
+      <div class="col-3">
+            <div class="ctr" style="margin-left:0px">
+              <img src="statics/img/group113.png" style="height: 80px" />
+            </div>
+      </div>
+    </div>
+  </q-card>
+</template>
+
+<script>
+export default {
+  props: {
+    page: "",
+    title: ""
+  },
+  methods: {
+    navigate() {
+      let self = this;
+      self.$router.push({ path: self.page });
+    }
+  }
+};
+</script>
