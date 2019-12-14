98aa2fefb1784987355b1ab5a0c1ff77fe24e56d
muhamadrizkiy <muhamadrizkiy@gmail.com>
2019-12-15 02:51:48 +0700

    register api integrated

--- src/api/auth/index.js
+++ src/api/auth/index.js
@@ -17,7 +17,20 @@ export default {
     registerUser(window, data) {
 
         return getApiNoAuth()
-            .post('/Users', data)
+            .post('/account', {
+                nama: data.nama,
+                password: "password",
+                notelp: data.notelp,
+                email: data.email,
+                alamat: data.alamat,
+                kodepos: data.kodepos,
+                sekolah: data.sekolah,
+                tingkat: data.tingkat,
+                kelas: data.kelas,
+                point: 0,
+                beA: data.beA,
+                toSay: data.toSay
+            })
                 .then(function(response) {
                     return response.data;
             })
--- src/pages/signin/biodata.vue
+++ src/pages/signin/biodata.vue
@@ -27,23 +27,23 @@
         </div>
         <q-form class="q-gutter-md">
           <p class="fusual">Nama</p>
-          <q-input rounded outlined size="md"/>
+          <q-input rounded outlined size="md" v-model="nama"/>
           <p class="fusual">No. Telp</p>
-          <q-input rounded outlined />
+          <q-input rounded outlined v-model="notelp"/>
           <p class="fusual">Email</p>
-          <q-input rounded outlined bg-color="grey-4" />
+          <q-input rounded outlined bg-color="grey-4" v-model="email"/>
           <div class="row">
             <div class="col-8">
               <p class="fusual2">Alamat</p>
-              <q-input rounded outlined />
+              <q-input rounded outlined v-model="alamat"/>
             </div>
             <div class="col">
               <p class="fusual2">Kode Pos</p>
-              <q-input rounded outlined />
+              <q-input rounded outlined v-model="kodepos"/>
             </div>
           </div>
           <p class="fusual">Sekolah</p>
-          <q-input rounded outlined />
+          <q-input rounded outlined v-model="sekolah"/>
           <div class="row">
             <div class="col">
               <p class="fusual2">Tingkat</p>
@@ -56,7 +56,7 @@
           </div>
 
           <center>
-            <q-btn unelevated rounded color="grey" label="Simpan" style="width: 90%" to="/preMenu"/>
+            <q-btn unelevated rounded color="grey" label="Simpan" style="width: 90%" @click="onSubmit()"/>
           </center>
         </q-form>
       </q-card-section>
@@ -74,9 +74,23 @@
 }
 </style>
 <script>
+
+import register from "../../api/auth/index";
 export default {
   data () {
     return {
+      data : {
+        nama: "a",
+        notelp: "a",
+        email: "a",
+        alamat: "a",
+        kodepos: "a",
+        sekolah: "a",
+        tingkat: "a",
+        kelas: "a",
+        beA: "a",
+        toSay: "a"
+      },
       model: null,
       model2:null,
       options: [
@@ -87,6 +101,41 @@ export default {
       ],
       
     }
+  },
+
+
+  methods: {
+    onSubmit() {    
+      const self = this
+      register.registerUser(window, self.data)
+          .then(function (result) {
+              if (!result) {
+                  self.$q.notify({
+                      color: "red-5",
+                      textColor: "white",
+                      icon: "fas fa-exclamation-triangle",
+                      message: "Failed to create data. Please Try Again!",
+                      position: "top",
+                      timeout: 500
+                  });
+          
+              } else {
+                  self.$q.notify({
+                      color: "green-4",
+                      textColor: "white",
+                      icon: "fas fa-check-circle",
+                      message: "Data has been create",
+                      position: "top",
+                      timeout: 500
+                  });
+                  
+                  self.$router.push('/preMenu')
+              }
+          })
+          .catch(function (err) {
+              console.log(err);
+          });
+      }
   }
 }
 </script>
