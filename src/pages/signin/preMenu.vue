967aa0cc17294509cb082198b11b7bcfe9211836
Gina Rizka Alsyah <ginarizkaa@gmail.com>
2019-12-15 13:17:08 +0700

    form revisi, height form

--- src/pages/signin/preMenu.vue
+++ src/pages/signin/preMenu.vue
@@ -16,6 +16,7 @@
         />
       </div>
       <q-btn
+        size="sm"
         unelevated
         rounded
         color="yellow"
@@ -28,18 +29,37 @@
     </center>
     <div class="ctr" style="margin-top:20px">
       <img src="statics/img/g4.png" style="height: 300px" class="q-px-sm" />
-      <img src="statics/img/g3.png" style="height: 300px" class="q-px-sm"  />
+      <img src="statics/img/g3.png" style="height: 300px" class="q-px-sm" />
     </div>
   </div>
 </template>
 <style lang="stylus">
-//modifikasi
-.q-field__control{
-  height:25px;
+// modifikasi
+.q-field__control {
+  height: 25px;
   font-family: 'NeutraTextBook';
 }
+
 .q-field__native, .q-field__prefix, .q-field__suffix {
   color: rgb(71, 96, 172);
+}
+
+.q-field--auto-height .q-field__native {
+  min-height: 40px;
+}
+
+.q-field--auto-height .q-field__control {
+  height: 40px;
+}
+
+.q-field--auto-height .q-field__control, .q-field--auto-height .q-field__native {
+  min-height: 40px;
+}
+
+.q-field__marginal {
+  height: 40px;
+  color: rgba(0, 0, 0, 0.54);
+  font-size: 24px;
 }
 </style>
 <script>
