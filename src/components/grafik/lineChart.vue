15da7d0e23c69307c57a3f71e8e49b27bcbe036c
Gina Rizka Alsyah <ginarizkaa@gmail.com>
2019-12-28 17:54:14 +0700

    komponen grafik, grafik rapor

---
+++ src/components/grafik/lineChart.vue
@@ -0,0 +1,43 @@
+<style>
+.chartjs-render-monitor{
+  background-color: #f8f8f8;
+}
+body {
+    font-family: 'NeutraText';
+}
+</style>
+<script>
+import { Line } from "vue-chartjs";
+
+export default {
+    extends: Line,
+    props:{
+        title:'',
+        labels:{
+            type: Array,
+            required: true
+        },
+        datasets: {
+            type: Array,
+            required: true
+        },
+    },
+    mounted() {
+        let self = this;
+        this.renderChart(
+            {
+                labels: self.labels,
+                datasets: self.datasets
+            },
+            {
+                responsive: true,
+                maintainAspectRatio: false,
+                title: {
+                    display: true,
+                    text: self.title
+                }
+            }
+        );
+    }
+};
+</script>
--- src/pages/rapor/grafik.vue
+++ src/pages/rapor/grafik.vue
@@ -1,39 +1,38 @@
 <template>
-  <div>
-    <div class="q-ma-md">
-      <q-card class="card q-mt-md q-mb-md">
-        <div class="row">
-          <div class="col">
-            <q-item>
-              <q-item-section class="txt-left font2">
-                <q-input
-                  borderless
-                  style="height:50px; margin-top:-10px"
-                  v-model="text"
-                  label="Cari Universitas/Jurusan"
-                />
-              </q-item-section>
-            </q-item>
-          </div>
-          <div class="col-2" style="text-align:right">
-            <img src="statics/img/g2.png" height="50px" width="45px" />
-          </div>
-          <div class="col-2" style="text-align:center">
-            <q-icon name="search" style="font-size: 45px; margin-top:5px" @click="searchUniv()" />
-          </div>
+  <div class="q-ma-md">
+    <q-card class="card q-mt-md q-mb-md">
+      <div class="row">
+        <div class="col">
+          <q-item>
+            <q-item-section class="txt-left font2">
+              <q-input
+                borderless
+                style="height:50px; margin-top:-10px"
+                v-model="text"
+                label="Cari Universitas/Jurusan"
+              />
+            </q-item-section>
+          </q-item>
         </div>
-      </q-card>
-      <div style="text-align:right">
-        <q-btn
-          rounded
-          class="font4"
-          color="secondary"
-          label="submit"
-          style="width:40%;text-align:right"
-          @click="sort2()"
-        />
+        <div class="col-2" style="text-align:right">
+          <img src="statics/img/g2.png" height="50px" width="45px" />
+        </div>
+        <div class="col-2" style="text-align:center">
+          <q-icon name="search" style="font-size: 45px; margin-top:5px" @click="searchUniv()" />
+        </div>
       </div>
+    </q-card>
+    <div style="text-align:right">
+      <q-btn
+        rounded
+        class="font4"
+        color="secondary"
+        label="submit"
+        style="width:40%;text-align:right"
+        @click="sort2()"
+      />
     </div>
+    <lineChart :title="title" :datasets="datasets" :labels="labels" />
   </div>
 </template>
 <style>
@@ -43,13 +42,35 @@
 }
 </style>
 <script>
-export default {
-    data () {
-        return {
-            text: ""
-        }
+import lineChart from "components/grafik/lineChart.vue";
 
-    },
+export default {
+  components: {
+    lineChart
+  },
+  data() {
+    return {
+      text: "",
+      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      datasets: [
+        {
+          label: "Nilai TO UTBK",
+          data: [2, 10, 5, 9, 0, 6, 20],
+          backgroundColor: "transparent",
+          borderColor: "rgba(116, 1, 110, 0.8)",
+          pointBackgroundColor: "rgba(15, 99, 128, 190)"
+        },
+        {
+          label: "Akuntansi UI",
+          data: [10, 2, 9, 5, 8, 14, 1],
+          backgroundColor: "transparent",
+          borderColor: "rgba(1, 116, 188, 0.5)",
+          pointBackgroundColor: "rgba(171, 71, 188, 1)"
+        }
+      ],
+      title: 'Nilai TO UTBK'
+    };
+  },
   methods: {
     searchUniv() {
       console.log("Test");
