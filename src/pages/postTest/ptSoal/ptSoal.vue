0f263b8d64433599d5ac6f47ac73474b8e57155c
Gina Rizka Alsyah <ginarizkaa@gmail.com>
2019-12-29 18:25:30 +0700

    soal swipe post test

--- src/pages/postTest/ptSoal/ptSoal.vue
+++ src/pages/postTest/ptSoal/ptSoal.vue
@@ -1,40 +1,52 @@
 <template>
   <div>
     <div style="background-image: url('statics/img/blue193.png'); height:180px;">
-      <div style="padding-top:75px; padding-left:30px;">
-        <p class="font3 yellow fs2">
-          BAB 1
-          <br />TERMODINAMIKA
-        </p>
+      <div style="padding:12px;">
+        <q-btn flat icon="img:statics/icon/back1.png" @click="$router.go(-1)" />
       </div>
-    </div>
-    <div class="q-ma-sm ctr">
-      <q-btn round color="yellow-14" disable class="q-ma-xs">
-        <div class="font3 dark-blue fs2">1</div>
-      </q-btn>
-      <q-btn round color="yellow-12" disable class="q-ma-xs">
-        <div class="font3 dark-blue fs2">2</div>
-      </q-btn>
-      <q-btn round color="yellow-12" disable class="q-ma-xs">
-        <div class="font3 dark-blue fs2">3</div>
+      <div class="headermargin font3 fs35 yellow">BAB 1</div>
+      <div class="headermargin2 font3 fs35 yellow">TERMODINAMIKA</div>
+      <q-btn size="xs" unelevated rounded color="yellow" style="margin-left:26px">
+        <countdown class="q-ma-xs" :initial-value="900" />
       </q-btn>
     </div>
-    <div class="q-mt-lg ctr">
-      <img src="statics/img/soal.png" />
-    </div>
-    <div class="q-mt-xl ctr">
-      <q-btn v-for="(item) in items" v-bind:key="item" color="blue-grey-4" class="q-ma-xs">
-        <div class="font3 dark-blue fs2">{{item}}</div>
-      </q-btn>
-    </div>
+    <soal jmlSoal="3" :soal="soal" />
   </div>
 </template>
-<style lang="stylus"></style>
+<style lang="stylus">
+.headermargin {
+  margin-top: -15px;
+  margin-left: 26px;
+}
+
+.headermargin2 {
+  margin-top: -5px;
+  margin-left: 26px;
+}
+</style>
 <script>
+import soal from "components/soal/cSoal.vue";
+import countdown from "components/countDown/cCountDown";
+
 export default {
+  components: {
+    soal,
+    countdown
+  },
   data() {
     return {
-      items: ["A", "B", "C", "D", "E"]
+      items: ["A", "B", "C", "D", "E"],
+      soal: [
+        {
+          img: "statics/img/soal.png"
+        },
+        {
+          img: "statics/img/g5.png"
+        },
+        {
+          img: "statics/img/g7.png"
+        }
+      ]
     };
   }
 };
